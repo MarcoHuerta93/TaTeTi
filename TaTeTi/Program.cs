0b98f27ab401bec5038b5118c8b550ba88832cfc
Marco Huerta <marco.huerta.sc@pm.me>
2021-07-24 15:05:37 -0500

    Creación de array para los valores del juego

--- TaTeTi/Program.cs
+++ TaTeTi/Program.cs
@@ -10,18 +10,28 @@ namespace TaTeTi
             Console.Read();
             
         }
+
+
+        //Array que contiene variables del juego
+        static char[,] tableroJuego =
+        {
+            {'1','2','3' },
+            {'4','5','6' },
+            {'7','8','9' }
+        };
+
         //Método crea el tablero
         public static void CreaTablero()
         {
             Console.WriteLine("_________________");
             Console.WriteLine("     |     |");
-            Console.WriteLine(" {0}   |  {1}  | {2}",  1, 2, 3);
+            Console.WriteLine(" {0}   |  {1}  |  {2}",  tableroJuego[0,0], tableroJuego[0,1], tableroJuego[0,2]);
             Console.WriteLine("_____|_____|_____");
             Console.WriteLine("     |     |");
-            Console.WriteLine(" {0}   |  {1}  | {2}", 1, 2, 3);
+            Console.WriteLine(" {0}   |  {1}  |  {2}", tableroJuego[1, 0], tableroJuego[1, 1], tableroJuego[1, 2]);
             Console.WriteLine("_____|_____|_____");
             Console.WriteLine("     |     |");
-            Console.WriteLine(" {0}   |  {1}  | {2}", 1, 2, 3);
+            Console.WriteLine(" {0}   |  {1}  |  {2}", tableroJuego[2, 0], tableroJuego[2, 1], tableroJuego[2, 2]);
             Console.WriteLine("_____|_____|_____");
         }
     }
