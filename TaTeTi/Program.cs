4039beec9204458baa934f38db7dd83bb2d8fc48
Marco Huerta <marco.huerta.sc@pm.me>
2021-07-24 14:42:48 -0500

    Inicializando Desafío

---
+++ TaTeTi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TaTeTi
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
